cacf9456ce119ef0b86c275005b71eb381d6cb2a
danielvportlandia <danielshelton90@gmail.com>
2018-06-21 11:11:28 -0700

    fixed signup tests.

--- src/__test__/lib/test.env.js
+++ src/__test__/lib/test.env.js
@@ -2,4 +2,4 @@ process.env.NODE_ENV = 'development';
 process.env.PORT = 5000;
 process.env.MONGODB_URI = 'mongodb://localhost/rs-testing';
 process.env.RS_SECRET = 'fake_super_secret';
-
+process.env.COOKIE_TIMEOUT = 900000;
--- src/route/shoes-request-form-router.js
+++ src/route/shoes-request-form-router.js
@@ -2,7 +2,6 @@
 
 import { Router } from 'express';
 import { json } from 'body-parser';
-import HttpError from 'http-errors';
 
 import Client from '../model/client';
 import bearerAuthMiddleware from '../lib/bearer-auth-middleware';
@@ -14,11 +13,6 @@ const jsonParser = json();
 const shoesRequestFormRouter = new Router();
 
 shoesRequestFormRouter.post('/request', bearerAuthMiddleware, jsonParser, (request, response, next) => {
-  // if (!request.account) {
-  //   console.log(request);
-  //   return next(new HttpError(400, 'SHOES REQUEST FORM - invalid request'));
-  // }
-  console.log('__REQBODY__', request.body);
   return Client.findOne({ account: request.account._id })
     .then((client) => {
       request.body.client = client._id;
@@ -32,7 +26,5 @@ shoesRequestFormRouter.post('/request', bearerAuthMiddleware, jsonParser, (reque
     })
     .catch(next);
 });
-
-// TODO: shoesRequestFormRouter.get('/shoes-request-forms', jsonParser, (request, response, next)
 
 export default shoesRequestFormRouter;
