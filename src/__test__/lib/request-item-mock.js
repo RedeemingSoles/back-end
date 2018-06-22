48d82e6c6355939ccc392128785dbec383f5fbd8
danielvportlandia <danielshelton90@gmail.com>
2018-06-21 18:20:58 -0700

    removing unused mocks

--- src/__test__/lib/request-item-mock.js
+++ src/__test__/lib/request-item-mock.js
@@ -1,26 +1,7 @@
 'use strict';
 
-import faker from 'faker';
-import { createShoesRequestFormMock, removeShoesRequestFormMock } from './shoes-request-form-mock';
+import { removeShoesRequestFormMock } from './shoes-request-form-mock';
 import RequestItem from '../../model/request-item';
-
-const createRequestItemMock = () => {
-  const resultMock = {};
-
-  return createShoesRequestFormMock()
-    .then((shoesRequestFormSetMock) => {
-      resultMock.shoesRequestFormSetMock = shoesRequestFormSetMock;
-      return new RequestItem({
-        childName: faker.name.firstName(),
-        shoeType: faker.lorem.word(),
-        age: 'youth',
-        gender: 'female',
-        shoeSize: faker.random.number(16),
-        message: faker.lorem.words(30),
-        shoesRequestForm: shoesRequestFormSetMock.shoesRequestForm._id,
-      }).save();
-    });
-};
 
 const removeRequestItemMock = () => {
   return Promise.all([
@@ -29,4 +10,4 @@ const removeRequestItemMock = () => {
   ]);
 };
 
-export { createRequestItemMock, removeRequestItemMock };
+export default removeRequestItemMock;
